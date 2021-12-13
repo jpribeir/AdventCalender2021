9650193cb3f8c285dce3d43c9dbc7c4afa1d176d
jpribeir <joaop.ribeiro@outlook.pt>
2021-12-13 01:05:08 +0000

    [WIP] Day 12 part1 finished, part 2 still debugging

--- src/day12.py
+++ src/day12.py
@@ -1,35 +1,4 @@
 # Day 12 of the 2021 Advent of Code
-class Cave():
-    # Initiate the object
-    def __init__(self,name,connect):
-        self.name = name
-        self.connect_list = []
-        self.addConnect(connect)
-        
-    def addConnect(self,connect):
-        self.connect_list.append(connect)
-
-# Convert input to a list of objects
-def OLDfile2list(input_filename):
-    with open(input_filename,"r") as input_file:
-        input_list = input_file.readlines()
-    cave_list = []
-    for line in input_list:
-        caveA = line.strip().split("-")[0]
-        caveB = line.strip().split("-")[1]
-        checkedA = False
-        checkedB = False
-        for i,cave in enumerate(cave_list):
-            if cave.name==caveA:
-                cave.addConnect(caveB)
-                checkedA = True
-            elif cave.name==caveB:
-                cave.addConnect(caveA)
-                checkedB = True
-        if not checkedA: cave_list.append(Cave(caveA,caveB))
-        if not checkedB: cave_list.append(Cave(caveB,caveA))
-    return cave_list
-
 # Convert input to a dict of lists
 def file2list(input_filename):
     with open(input_filename,"r") as input_file:
@@ -38,44 +7,70 @@ def file2list(input_filename):
     for line in input_list:
         caveA = line.strip().split("-")[0]
         caveB = line.strip().split("-")[1]
-        checkedA = False
-        checkedB = False
-        for cave in cave_dict:
-            if cave==caveA:
-                cave_dict[cave].append(caveB)
-                checkedA = True
-            elif cave==caveB:
-                cave_dict[cave].append(caveA)
-                checkedB = True
-            if checkedA and checkedB: break
-        else:
-            if not checkedA: cave_dict[caveA] = [caveB]
-            if not checkedB: cave_dict[caveB] = [caveA]
+        if caveB!="start":
+            try:
+                cave_dict[caveA].append(caveB)
+            except:
+                cave_dict[caveA] = [caveB]
+        if caveA!="start":
+            try:
+                cave_dict[caveB].append(caveA)
+            except:
+                cave_dict[caveB] = [caveA]
+        
     return cave_dict
 
-def enterCave(here,cave_dict,visited_list):
+def enterCavePart1(here,cave_dict,visited_list,ident):
+    print("%s%s"%(ident,visited_list))
     path_count = 0
-    print(visited_list)
     for cave in cave_dict[here]:
+        aux_list = visited_list.copy()
         if cave.isupper():
-            aux_list = visited_list.copy()
             aux_list.append(cave)
-            path_count += enterCave(cave,cave_dict,aux_list)
-        elif cave.islower():
-            if cave=="end":
-                return 1
-            elif cave not in visited_list:
-                aux_list = visited_list.copy()
-                aux_list.append(cave)
-                path_count += enterCave(cave,cave_dict,aux_list)
-                visited_list = aux_list.copy()
+            path_count += enterCavePart1(cave,cave_dict,aux_list,ident+":")
+        elif cave=="end":
+            visited_list.append(cave)
+            print("%s%s<---"%(ident,visited_list))
+            path_count += 1
+        elif cave not in visited_list:
+            aux_list.append(cave)
+            path_count += enterCavePart1(cave,cave_dict,aux_list,ident+":")
+    print("%s%s paths found on this level"%(ident,path_count))
+    return path_count
+
+def enterCave(here,cave_dict,visited_list,ident,lowmax):
+    #print("%s%s"%(ident,visited_list))
+    path_count = 0
+    lower_dict = {cave:visited_list.count(cave) for cave in cave_dict[here] if cave.islower()}
+    for cave in cave_dict[here]:
+        aux_list = visited_list.copy()
+        print("%s%s"%(ident,lower_dict))
+        if cave.isupper():
+            aux_list.append(cave)
+            path_count += enterCave(cave,cave_dict,aux_list,ident+":",lowmax)
+        elif cave=="end":
+            visited_list.append(cave)
+            print("%s%s<---"%(ident,visited_list))
+            #print("%sReached end at %s!"%(ident,here))
+            path_count += 1
+        elif not lowmax in lower_dict.values():
+            aux_list.append(cave)
+            path_count += enterCave(cave,cave_dict,aux_list,ident+":",lowmax)
+        elif cave not in visited_list:
+            aux_list.append(cave)
+            path_count += enterCave(cave,cave_dict,aux_list,ident+":",lowmax)
+    #print("%s%s paths found on this level"%(ident,path_count))
     return path_count
 
 def part1(cave_dict):
-    return enterCave("start",cave_dict,["start"])
+    return enterCavePart1("start",cave_dict,["start"],"")
+
+def part2(cave_dict):
+    return enterCave("start",cave_dict,["start"],"",2)
 
 # Read input file
 cave_dict = file2list("../include/input12.inc")
-#example_list = file2list("../include/example12.inc")
-print("----DAY 12----\nPart1: %s"%part1(cave_dict))
-#print("Part2: %s"%part2(out_map,100))
+example_dict = file2list("../include/example12.inc")
+#print(example_dict)
+#print("----DAY 12----\nPart1: %s"%part1(example_dict))
+print("Part2: %s"%part2(example_dict))
