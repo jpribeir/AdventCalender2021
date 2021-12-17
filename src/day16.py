2dcc532ea6afa69362a9ca20bbd35b0900d4b3b5
jpribeir <joaop.ribeiro@outlook.pt>
2021-12-17 19:22:52 +0000

    Day 16 parts 1 and 2 finished, with comments

--- src/day16.py
+++ src/day16.py
@@ -4,15 +4,13 @@ def file2list(input_filename):
     with open(input_filename,"r") as input_file:
         return input_file.read().strip()
 
-def getValueListed(val):
-    return [val[0+i:5+i] for i in range(0,len(val),5)]
-
+# Each time a packet header is found, this function is called recursively
 def decodePacket(packet_B):
     # Store this packet's version, typeID
     version = int(packet_B[0:3],2)
     typeID = int(packet_B[3:6],2)
     rest_of_packet = packet_B[6:]
-
+    
     # Type=4: packet is a number
     if typeID==4:
         num_B = ""
@@ -34,7 +32,7 @@ def decodePacket(packet_B):
             while packet:
                 sub_version,number,packet = decodePacket(packet)
                 version += sub_version
-                if number: number_list.append(number)
+                number_list.append(number)
         
         # LengthID=1: next 11 bits are the number of packets to parse
         else:
@@ -55,28 +53,12 @@ def decodePacket(packet_B):
         elif typeID==5: result = int(number_list[0]>number_list[1])
         elif typeID==6: result = int(number_list[0]<number_list[1])
         elif typeID==7: result = int(number_list[0]==number_list[1])
-    #### DEBUG PRINTS ####
-    print("----------------------------------------------")
-    print("typeID: %s"typeID)
-    if typeID!=4:print(number_list)
-    print("-> %s"%result)
-    ######################
     return version,result,rest_of_packet
 
 def part1(tmsn_H):
     return decodePacket(str(bin(int(tmsn_H,16))[2:].zfill(len(tmsn_H)*4)))
 
 # Read input file
-#transmission = file2list("../include/input16.inc")
-transmission = file2list("../include/input_ricardo16.inc")
-example1 = "C200B40A82"
-example2 = "04005AC33890"
-example3 = "880086C3E88112"
-example4 = "CE00C43D881120"
-example5 = "D8005AC2A8F0"
-example6 = "F600BC2D8F"
-example7 = "9C005AC2F8F0"
-example8 = "9C0141080250320F1802104A08"
-version,result2,_ = part1(transmission)
+version,result2,_ = part1(file2list("../include/input16.inc"))
 print("----DAY 16----\nPart1: %s"%version)
 print("Part2: %s"%result2)
