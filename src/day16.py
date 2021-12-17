4807e0e72b5f138f033dcb282d4fda0db808cbe5
Joao Ribeiro <jpribeir@synopsys.com>
2021-12-17 16:26:52 +0000

    [WIP] Cleaned up the code but still not returning correct result for part2

--- src/day16.py
+++ src/day16.py
@@ -7,62 +7,43 @@ def file2list(input_filename):
 def getValueListed(val):
     return [val[0+i:5+i] for i in range(0,len(val),5)]
 
-def hexaToBin(val_H):
-    return str(bin(int(val_H,16))[2:].zfill(len(val_H)*4))
-
-def versionControl(packet_B,ident):
-    global print_en
-    print_en = 0
-    ident += ":"
-    if print_en: print("------------------------------"*4)
-    if print_en: print(packet_B)
-
-    # If sending the zeros at the end of a value it can return 0
-    if "1" not in packet_B: return False,""
-    global result1
-    result1 += int(packet_B[0:3],2)
+def decodePacket(packet_B):
+    # Store this packet's version, typeID
+    version = int(packet_B[0:3],2)
     typeID = int(packet_B[3:6],2)
-    value = packet_B[6:]
-    if print_en: print("Version is %s ------- Total is %s"%(int(packet_B[0:3],2),result1))
-    if print_en: print("Type is %s"%typeID)
+    rest_of_packet = packet_B[6:]
 
-    # Type=4: value is a list of numbers
+    # Type=4: packet is a number
     if typeID==4:
-        val_list = getValueListed(value)
-        number_B = ""
-        for chunk in val_list.copy():
-            number_B = number_B + val_list.pop(0)[1:]
-            if chunk[0]=="0": break
-        if print_en: print("Number is %s"%number_B)
-        return int(number_B,2),"".join(val_list)
+        num_B = ""
+        while rest_of_packet[0]!="0":
+            num_B += rest_of_packet[1:5]
+            rest_of_packet = rest_of_packet[5:]
+        result = int(num_B+rest_of_packet[1:5],2)
+        rest_of_packet = rest_of_packet[5:]
 
-    # Type!=4: value is an operand
+    # Type!=4: packet is an operation
     else:
         number_list = []
-
         # LengthID=0: next 15 bits are the length of bits to parse
-        if value[0] == "0":
-            values_length = int(value[1:16],2)
-            if print_en: print("%s -> Length is %s"%(value[0],values_length))
-            packet = value[16:16+values_length]
-            rest_of_packet = value[16+values_length:]
-            if print_en: print("Packet is %s"%packet)
-            if print_en: print("Rest is |%s|"%rest_of_packet)
+        if rest_of_packet[0]=="0":
+            values_length = int(rest_of_packet[1:16],2)
+            rest_of_packet = rest_of_packet[16:]
+            packet = rest_of_packet[:values_length]
+            rest_of_packet = rest_of_packet[values_length:]
             while packet:
-                read_number,packet = versionControl(packet,ident)
-                if read_number: number_list.append(read_number)
+                sub_version,number,packet = decodePacket(packet)
+                version += sub_version
+                if number: number_list.append(number)
         
         # LengthID=1: next 11 bits are the number of packets to parse
         else:
-            values_count = int(value[1:12],2)
-            if print_en: print("%s -> Count is %s"%(value[0],values_count))
-            packet = value[12:]
-            if print_en: print("Rest is %s"%packet)
-            for num in range(values_count):
-                read_number,packet = versionControl(packet,ident)
-                number_list.append(read_number)
-            rest_of_packet = packet
-        if print_en: print("Rest is %s"%rest_of_packet)
+            values_count = int(rest_of_packet[1:12],2)
+            rest_of_packet = rest_of_packet[12:]
+            for _ in range(values_count):
+                sub_version,number,rest_of_packet = decodePacket(rest_of_packet)
+                version += sub_version
+                number_list.append(number)
         
         # Choose operation
         if typeID==0: result = sum(number_list)
@@ -74,25 +55,28 @@ def versionControl(packet_B,ident):
         elif typeID==5: result = int(number_list[0]>number_list[1])
         elif typeID==6: result = int(number_list[0]<number_list[1])
         elif typeID==7: result = int(number_list[0]==number_list[1])
-        if print_en: print("%s: type->%s"%(ident,typeID))
-        if print_en: print("%s%s"%(ident,result))
-        return result,rest_of_packet
+    #### DEBUG PRINTS ####
+    print("----------------------------------------------")
+    print("typeID: %s"typeID)
+    if typeID!=4:print(number_list)
+    print("-> %s"%result)
+    ######################
+    return version,result,rest_of_packet
 
 def part1(tmsn_H):
-    tsmn_B = hexaToBin(tmsn_H)
-    return versionControl(tsmn_B,"")
+    return decodePacket(str(bin(int(tmsn_H,16))[2:].zfill(len(tmsn_H)*4)))
 
 # Read input file
-transmission = file2list("../include/input16.inc")
-result1 = 0
-example = "C200B40A82"
-example = "04005AC33890"
-example = "880086C3E88112"
-example = "CE00C43D881120"
-example = "D8005AC2A8F0"
-example = "F600BC2D8F"
-example = "9C005AC2F8F0"
-example = "9C0141080250320F1802104A08"
-result2,no = part1(transmission)
-print("----DAY 16----\nPart1: %s"%result1)
+#transmission = file2list("../include/input16.inc")
+transmission = file2list("../include/input_ricardo16.inc")
+example1 = "C200B40A82"
+example2 = "04005AC33890"
+example3 = "880086C3E88112"
+example4 = "CE00C43D881120"
+example5 = "D8005AC2A8F0"
+example6 = "F600BC2D8F"
+example7 = "9C005AC2F8F0"
+example8 = "9C0141080250320F1802104A08"
+version,result2,_ = part1(transmission)
+print("----DAY 16----\nPart1: %s"%version)
 print("Part2: %s"%result2)
