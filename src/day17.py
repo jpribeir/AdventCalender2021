b6ba9749d560fd345faf87fd787f77f6b5111f07
jpribeir <joaop.ribeiro@outlook.pt>
2021-12-19 12:32:14 +0000

    [WIP] Day 17 in progress, only done input read

--- src/day17.py
+++ src/day17.py
@@ -3,13 +3,15 @@
 def file2list(input_filename):
     with open(input_filename,"r") as input_file:
         line =  input_file.read().strip()
-        x_line = (line.split(":")[1]).split(",")[0]
-        y_line = (line.split(":")[1]).split(",")[1]
-    
-
+    x_line = (line.split(":")[1]).split(",")[0]
+    y_line = (line.split(":")[1]).split(",")[1]
+    xrange = list(map(int,x_line.split("=")[1].split("..")))
+    yrange = list(map(int,y_line.split("=")[1].split("..")))
+    return xrange,yrange
 
 # Read input file
-transmission = file2list("../include/input17.inc")
-
-print("----DAY 16----\nPart1: %s"%result1)
+xrange,yrange = file2list("../include/input17.inc")
+print(list(range(xrange[0],xrange[1]+1)))
+print(list(range(yrange[0],yrange[1]+1)))
+#print("----DAY 16----\nPart1: %s"%result1)
 #print("Part2: %s"%result2)
